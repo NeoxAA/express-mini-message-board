79810f3cc5f5147eb3d29c9ce871159bc0ffe0bb
Asam Afzal <aainvests@outlook.com>
2023-06-25 15:33:04 -0400

    updated to connect to db

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,27 @@
 var express = require('express');
 var router = express.Router();
 let mysql = require('mysql');
+var Filter = require('bad-words'),
+filter = new Filter();
 
-
-let connection = mysql.createConnection({
+let pool = mysql.createPool({
+  connectionLimit : 10,
   host: 'localhost',
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME
 });
 
-
-
-var Filter = require('bad-words'),
-filter = new Filter();
-
-const messages = [
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Mini Message Board", messages: messages })
+  pool.query('SELECT * FROM Messages', (err, rows) => {
+    if(err){
+      console.error(err.message);
+      res.send({error:err});
+    } else {
+      res.render('index', {title: "Mini Message Board", messages:rows})
+    }
+  });
 });
 
 router.get('/new', function(req,res,next) {
@@ -35,22 +30,28 @@ router.get('/new', function(req,res,next) {
 
 router.post("/new", function(req,res,next){
   if (req.body.user && req.body.message) {
-    connection.connect(function(err) {
-      if (err) {
-        return console.error('error: ' + err.message);
+    let cleanMessage = filter.clean(req.body.message);
+    let user = req.body.user;
+    let added = new Date();
+
+    pool.query(
+      'INSERT INTO Messages (text, user, added) VALUES (?, ?, ?)',
+      [cleanMessage, user, added],
+      (err, results) => {
+        if(err){
+          console.log('error: ', +err.message);
+          res.send({error: err});
+        } else {
+          console.log('Row inserted:' + results.affectedRows);
+          res.redirect('/');
+        }
       }
-      let cleanMessage = filter.clean(req.body.message);
-      messages.push({text: cleanMessage, user: req.body.user, added: new Date()});
-      res.redirect('/')
-      console.log('Connected to the MySQL server.');
-    });
+    );
 
-  }
-  else {
-    console.error("Missing fields");
-    res.render('form', {title: "Mini Message Board", errorMessage: "Missing fields"});
+  } else {
+    res.status(400).send('Invalid Request');
   }
 
-})
+});
 
 module.exports = router;
